385c6d56ec83315d924470eb4cc11de9872105ba
Felmeta Abate <abatefelmeta43@gmail.com>
2021-06-17 05:09:41 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * main - print string
+ *Description: prints "\" programming is like building a multilingual puzzel
+ * return: 0
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzlel");
